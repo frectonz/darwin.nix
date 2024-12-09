6fb1a6823c5258d03c5713b7eb01279bb03d6cc7
frectonz <fraol0912@gmail.com>
2024-12-09 14:19:02 +0300

    feat: add password store

--- home/config.nix
+++ home/config.nix
@@ -1,11 +1,17 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   imports = [ ./shell.nix ./terminals.nix ./editors.nix ];
 
-  services.gpg-agent = {
-    enable = true;
-  };
+  services.gpg-agent.enable = true;
   programs.gpg.enable = true;
   programs.fish.loginShellInit = "gpgconf --launch gpg-agent";
+
+  programs.password-store = {
+    enable = true;
+    settings = {
+      PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";
+      PASSWORD_STORE_KEY = "9CFA458945B7094F";
+    };
+  };
 
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
