2260f408558b7382533d85da74192c602c4da8e2
frectonz <fraol0912@gmail.com>
2026-05-02 03:17:32 +0300

    feat: flake-only nix config and network tuning

--- hosts/maxwell/darwin-configuration.nix
+++ hosts/maxwell/darwin-configuration.nix
@@ -1,6 +1,8 @@
 {
   flake,
   perSystem,
+  lib,
+  config,
   pkgs,
   ...
 }:
@@ -17,8 +19,24 @@
 
   fonts.packages = with pkgs; [ nerd-fonts.fira-code ];
 
-  nix.settings.experimental-features = "nix-command flakes";
-  nix.settings.sandbox = true;
+  nix =
+    let
+      flakeInputs = lib.filterAttrs (_: lib.isType "flake") flake.inputs;
+    in
+    {
+      settings = {
+        experimental-features = "nix-command flakes";
+        sandbox = true;
+        flake-registry = "";
+        nix-path = config.nix.nixPath;
+        connect-timeout = 10;
+        http-connections = 25;
+        download-attempts = 5;
+      };
+      channel.enable = false;
+      registry = lib.mapAttrs (_: f: { flake = f; }) flakeInputs;
+      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    };
 
   users.users.frectonz = {
     name = "frectonz";
