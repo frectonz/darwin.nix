eeb383eaf60328a0786fe4c158b570d03a2f9bee
frectonz <fraol0912@gmail.com>
2026-06-09 17:36:55 +0300

    fix: only show jj-starship segment in jj repos

--- hosts/maxwell/users/frectonz/starship.nix
+++ hosts/maxwell/users/frectonz/starship.nix
@@ -1,6 +1,24 @@
+{ pkgs, ... }:
+let
+  jjDetect = pkgs.writeShellScript "jj-detect" ''
+    dir="$PWD"
+    while :; do
+      [ -d "$dir/.jj" ] && exit 0
+      [ "$dir" = "/" ] && break
+      dir=$(dirname "$dir")
+    done
+    exit 1
+  '';
+in
 {
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    extraPackages = [ pkgs.jj-starship ];
+    settings = {
+      custom.jj.when = "${jjDetect}";
+      custom.jj.command = "jj-starship";
+      custom.jj.format = "$output ";
+    };
   };
 }
