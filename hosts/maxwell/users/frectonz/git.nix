4bb33a0455e1a37db0a8cadbc32daa0c5d8b18d6
frectonz <fraol0912@gmail.com>
2026-07-21 11:01:04 +0300

    fix: better git fetching config

--- hosts/maxwell/users/frectonz/git.nix
+++ hosts/maxwell/users/frectonz/git.nix
@@ -8,6 +8,13 @@
       init.defaultBranch = "main";
       diff.external = "difft";
       gpg.program = "${config.programs.gpg.package}/bin/gpg2";
+      http = {
+        version = "HTTP/1.1";
+        postBuffer = 1073741824; # 1GB buffer size
+        lowSpeedLimit = 0;
+        lowSpeedTime = 999999;
+      };
+
     };
     signing = {
       key = "9CFA458945B7094F";
