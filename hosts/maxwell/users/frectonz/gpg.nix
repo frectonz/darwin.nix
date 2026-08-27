fc836d6564e00555c119381b51182dd482fc9730
frectonz <fraol0912@gmail.com>
2026-08-28 01:41:44 +0300

    fix: setup gpg for mac

--- hosts/maxwell/users/frectonz/gpg.nix
+++ hosts/maxwell/users/frectonz/gpg.nix
@@ -1,5 +1,12 @@
+{ pkgs, ... }:
 {
-  services.gpg-agent.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    pinentry.package = pkgs.pinentry_mac;
+  };
   programs.gpg.enable = true;
   programs.fish.loginShellInit = "gpgconf --launch gpg-agent";
+
+  # Don't store the GPG passphrase in the macOS keychain
+  targets.darwin.defaults."org.gpgtools.common".UseKeychain = false;
 }
